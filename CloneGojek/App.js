928e8d6e0ba0c59b1a17f36598d28e5c1444d0c4
NuryaFahruRosyidin2406 <81367105+NuryaFahruRosyidin2406@users.noreply.github.com>
2021-11-03 16:14:01 +0700

    Update App.js

--- CloneGojek/App.js
+++ CloneGojek/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Image, TextInput, ScrollView} from 'react-native';
+import {Platform, StyleSheet, Text, View, Image, TextInput, ScrollView, Button, TouchableOpacity} from 'react-native';
 
 export default class App extends Component {
   render(){
@@ -100,12 +100,19 @@ export default class App extends Component {
           <View style={{height: 17, backgroundColor: '#F2F2F4', marginTop: 20}}></View>
           {/*News Section*/}
           <View style={{paddingTop: 16, paddingHorizontal: 16}}>
-            <View>
+            <View style={{position: 'relative'}}>
               <Image source={require('./dummy/sepak-bola.jpg')} style={{height: 200, width: '100%', borderRadius: 6}}/>
+              <View style={{width: '100%', height: '100%', position: 'absolute', top: 0, left: 0, backgroundColor: 'black', opacity: 0.2, borderRadius: 6}}></View>
+              <View style={{height: 15, width: 60, position: 'absolute', top: 16, left: 16}}>
+                <Image source={require('./logo/white.png')} style={{width: undefined, height: undefined, resizeMode: 'contain', flex: 1}}/>
+              </View>
             </View>
-            <View style={{paddingTop: 16, paddingBottom: 20}}>
+            <View style={{paddingTop: 16, paddingBottom: 20, borderBottomColor: '#E8E9ED', borderBottomWidth: 1, marginBottom: 20}}>
               <Text style={{fontSize: 16, fontWeight: 'bold', color: '#1C1C1C'}}>GO-NEWS</Text>
-              <Text style={{fontSize: 14, fontWeight: 'normal', color: '#7A7A7A'}}>Dimas Drajet selamatkan penalti, Timnas U-23 kalahkan Brunei</Text>
+              <Text style={{fontSize: 14, fontWeight: 'normal', color: '#7A7A7A', marginBottom: 11}}>Dimas Drajet selamatkan penalti, Timnas U-23 kalahkan Brunei</Text>
+              <TouchableOpacity style={{backgroundColor: '#61A756', paddingHorizontal: 12, paddingVertical:11, alignSelf: 'flex-end', borderRadius: 4}}>
+                <Text style={{fontSize: 13, fontWeight: 'bold', color: 'white', textAlign: 'center'}}>READ</Text>
+              </TouchableOpacity>
             </View>
           </View>
         </ScrollView>
